ec0c9275726b6672682f151ee457a4ff4d30d3fa
Andrey Yamanov <tenphi@gmail.com>
2026-03-05 12:58:40 +0100

    feat: github packages

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import json from '@eslint/json';
+
+export default [
+  {
+    plugins: { json },
+    files: ['**/*.json'],
+    language: 'json/json',
+    rules: {
+      'json/no-duplicate-keys': 'error',
+    },
+  },
+];
